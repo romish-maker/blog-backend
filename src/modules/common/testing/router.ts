9e325bbfc632da2a63e328df9754e14cf8c5210f
RomishKuvatov <romish.kuvatov@tele2.kz>
2024-03-04 21:19:03 +0500

    added users in delete all data

--- src/modules/common/testing/router.ts
+++ src/modules/common/testing/router.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import {HttpStatusCode} from "../enums/HttpsStatusCodes";
-import {blogsCollection, postsCollection} from "../../../app/config/db";
+import {blogsCollection, postsCollection, usersCollection} from "../../../app/config/db";
 
 export const testingRouter = Router()
 
 testingRouter.delete('/all-data', async (req, res) => {
     await blogsCollection.deleteMany({})
     await postsCollection.deleteMany({})
+    await usersCollection.deleteMany({})
 
     res.sendStatus(HttpStatusCode.NO_CONTENT_204)
 })
